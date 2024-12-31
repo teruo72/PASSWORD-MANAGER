35f46b8f865e78b4c288a5eafffa4223a1724df7
inui <cinnamoroll2247@gmail.com>
2024-12-31 07:43:29 +0000

    Add echo>>.txt

--- PASSWORD-MANAGER.sh
+++ PASSWORD-MANAGER.sh
@@ -3,14 +3,15 @@
 
 echo パスワードマネージャーへようこそ！
 
-echo "サービス名を入力してください："
+echo "サービス名を入力してください:"
 read service
+
 echo "ユーザー名を入力してください："
 read user
+
 echo "パスワードを入力してください："
 read password
-
-touch step1
+echo "$service":"$user":"$password" >> data.txt
 
 # 入力が完了したら
 echo  Thank you!
